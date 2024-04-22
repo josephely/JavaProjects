370c9952daee3e418e64dab60c3fc24e96d99f12
Joseph Ely <joseph@solzorro.com>
2024-04-22 17:02:14 -0600

    hey dude

--- QuickStart.java
+++ QuickStart.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 class QuickStart {
 
     public static int add(int num1, int num2) {
@@ -15,12 +18,28 @@ class QuickStart {
     }
 
     public static void guessingGame() {
+        Scanner s = new Scanner(System.in);
+        boolean gameOver = false;
+        Random r = new Random(100);
+        int target = r.nextInt();
+        int turns = 10;
+        while (!gameOver || turns == 0) {
+            System.out.println("Enter a number");
+            int num = Integer.parseInt( s.nextLine());
+            if (num == target) gameOver = true;
+            else if (num < target) System.out.println("Too low, try again!");
+            else System.out.println("Too high, try again");
+            turns--;
+        }
+        
+
         
     }
 
     public static void main (String[] args) {
         //System.out.println(add(1,2));
-        System.out.println(reverse("chungus"));
+        //System.out.println(reverse("chungus"));
+        guessingGame();
     }
 }
 
